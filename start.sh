f66b866df30f02803415278f4c5f927a678dcc74
Udayg3417 <udayg3417@gmail.com>
2024-07-08 17:08:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Udayg3417/My-Auto-Filter-Bot
+  git clone https://github.com/Udayg3417/My-Auto-Filter-Bot /Eva
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eva
